b75c19d9dc325dd18540e96f10c60fcdd12a7074
Lukas Vavrek <lvavrek7@gmail.com>
2022-07-24 01:48:08 +0200

    Implemented general idea of polymorphic deserializer.

---
+++ PolymorphicDeserializerTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+using System.Text.Json;
+using Xunit;
+using PolymorphicDeserializer;
+
+namespace PolymorphicDeserializerTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Deserialize_ShouldUseCorrectType_Student()
+    {
+        const string json = @"{
+  ""Type"" : ""Student"",
+  ""Name"" : ""test name"",
+  ""Correctness"" : 97.65
+}";
+
+        var content = JsonSerializer.Deserialize<RegistryContent>(json);
+
+        Assert.IsType<StudentRegistryContent>(content);
+        Assert.Equal(RegistryType.Student, content.Type);
+        
+        Assert.Equal("test name", ((StudentRegistryContent)content).Name);
+        Assert.Equal(97.65, ((StudentRegistryContent)content).Correctness, 2);
+    }
+    
+    [Fact]
+    public void Deserialize_ShouldUseCorrectType_Book()
+    {
+        const string json = @"{
+  ""Type"" : ""Book"",
+  ""Title"" : ""eragon"",
+  ""Author"" : ""christopher paolini""
+}";
+
+        var content = JsonSerializer.Deserialize<RegistryContent>(json);
+
+        Assert.IsType<BookRegistryContent>(content);
+        Assert.Equal(RegistryType.Book, content.Type);
+        
+        Assert.Equal("eragon", ((BookRegistryContent)content).Title);
+        Assert.Equal("christopher paolini", ((BookRegistryContent)content).Author);
+    }
+
+    [Fact]
+    public void Service_GetRegistriesOfType_ShouldReturnCorrectType()
+    {
+        var registry = new Service()
+            .GetRegistryOfType<StudentRegistryContent>();
+
+        Assert.IsType<Registry<StudentRegistryContent>>(registry);
+        Assert.Equal("test name", registry.Content.Name);
+        Assert.Equal(97.65, registry.Content.Correctness, 2);
+    }
+}
